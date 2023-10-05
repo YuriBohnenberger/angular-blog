f976b62891def787e216bd0f99b14b080755fe7c
Yuri Bohnenberger <yuribohnenberger@gmail.com>
2023-10-05 20:58:57 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"CAMALEÃO CAMINHONEIRO",
+    "description": "Camaleão cominhoneiro posa para foto no seu big truck em posto de gasolina",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxw7_uVmQc3rnQVMuHVrrjK1Vv3D1SgQlL3w&usqp=CAU"
+  },
+  {
+    "id":"2",
+    "title":"CAMALEÃO MUDOU DE COR",
+    "description": "Camaleão cansa de ser confundido com o ambiente e muda de cor para se destacar",
+    "photoCover": "https://news.wttw.com/sites/default/files/field/image/Chameleon%20hero_0622.jpg"
+  }
+];
